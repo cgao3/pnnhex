ab36dce27cad57927f0b35959aa3b2b59021edee
CHAO <cgao3@ualberta.ca>
2016-06-20 17:04:23 -0600

    learning rate decay

--- Test.py
+++ Test.py
@@ -9,8 +9,20 @@ import numpy as np
 from layer import Layer
 from read_data import *
 
+num_epochs=10
+train_step=10
+
+tf.app.flags.DEFINE_boolean("training", True, "False if for evaluation")
+tf.app.flags.DEFINE_string("check_point_dir","savedModel/", "path to save the model")
+tf.app.flags.DEFINE_string("train_data_dir","data/","path to training data")
+tf.app.flags.DEFINE_string("test_data_dir", "test/","path to test data")
+FLAGS=tf.app.flags.FLAGS
+
+def error_rate(predictions, labels):
+    return 100.0- 100.0 * np.sum(np.argmax(predictions, 1) == labels)/predictions.shape[0]
+
 def main(argv=None):
-    read_raw_data("data/train_games.dat")
+
     train_data_node=tf.placeholder(tf.float32, shape=(BATCH_SIZE, INPUT_WIDTH, INPUT_WIDTH, INPUT_DEPTH))
     train_labels_node=tf.placeholder(tf.int32, shape=(BATCH_SIZE,))
     
@@ -43,25 +55,41 @@ def main(argv=None):
     logits = conv8.one_filter_out(out_conv7, BOARD_SIZE)
     print("logits", logits)
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits, train_labels_node))
-    
-    learning_rate = 0.01
+
+    train_prediction=tf.nn.softmax(logits)
+
+    batch=tf.Variable(0)
+    learning_rate=tf.train.exponential_decay(0.01,batch*BATCH_SIZE, train_step, 0.95,staircase=True)
     
     opt = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+    saver=tf.train.Saver()
 
     with tf.Session() as sess:
         tf.initialize_all_variables().run()
         print("Initialized!")
-        print("loss looks ", loss)
+        if not tf.app.flags.FLAGS.training:
+            ckpt = tf.train.get_checkpoint_state(FLAGS.check_point_dir)
+            if ckpt and ckpt.model_checkpoint_dir_path:
+                print("restoring a model")
+                saver.restore(sess,ckpt.model_checkpoint_dir_path)
+
+        read_raw_data("data/train_games.dat")
         offset1, offset2 = 0, 0
-        while(nEpoch < 2):
+        step=1
+        training_step=10000
+        while(nEpoch < num_epochs):
             off1, off2 = prepare_batch(offset1, offset2)
             x = batch_states.astype(np.float32)
             y = batch_labels.astype(np.int32)
             feed_diction = {train_data_node:x, 
                             train_labels_node:y}
-            _, loss_v=sess.run([opt,loss], feed_dict=feed_diction)
-            print("loss: ", loss_v)
-            offset1, offset2 = off1, off2
+            _, loss_v, predictions=sess.run([opt,loss, train_prediction], feed_dict=feed_diction)
+            print("epoch:", nEpoch, "loss: ", loss_v, "error rate:", error_rate(predictions, batch_labels))
+            offset1, offset2 = off1,off2
+            step = step + 1
+
+        tf.save(sess,FLAGS.check_point_dir+"/model.ckpt")
+
             
 if __name__ == "__main__":
     tf.app.run()

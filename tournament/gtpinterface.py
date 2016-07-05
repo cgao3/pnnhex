2cfdb29b838b721855dfbc6d2448ab160e3fcdbc
Chao Gao <cgao3@ualberta.ca>
2016-07-05 11:48:56 -0600

    tournament files moved to subdirectory

---
+++ tournament/gtpinterface.py
@@ -0,0 +1,86 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+sys.path.append("..")
+
+from game_util import *
+
+class gtpinterface(object):
+
+    def __init__(self, agent):
+        self.agent=agent
+        commands={}
+        commands["name"]=self.gtp_name
+        commands["genmove"]=self.gtp_genmove
+        commands["quit"]=self.gtp_quit
+        commands["showboard"]=self.gtp_show
+        commands["play"]=self.gtp_play
+        commands["list_commands"]=self.gtp_list_commands
+        commands["clear_board"]=self.gtp_clear
+        commands["gamestatus"]=self.gtp_gamestatus
+
+        self.commands=commands
+
+    def send_command(self, command):
+        p=command.split()
+        func_key=p[0]
+        args=p[1:]
+        assert(func_key in self.commands.keys())
+        #call that function with parameters
+        return self.commands[func_key](args)
+
+    def gtp_name(self, args):
+        return True, self.agent.agent_name
+
+    def gtp_list_commands(self, args):
+        return True, self.commands.keys()
+
+    def gtp_quit(self, args):
+        sys.exit()
+
+    def gtp_clear(self, args):
+        self.agent.initialize_game()
+        return True,""
+
+    def gtp_play(self, args):
+        #play black/white
+        assert(len(args)==2)
+        intmove=raw_move_to_int(args[1])
+        assert(0 <= intmove <BOARD_SIZE*BOARD_SIZE)
+        black_player=0
+        white_player=1
+        if intmove in self.agent.game_state:
+            return False, "INVALID! Occupied position."
+
+        if args[0][0]=='b':
+            self.agent.play_move(black_player, intmove)
+        elif args[0][0]=='w':
+            self.agent.play_move(white_player, intmove)
+        else:
+            return False, "player should be black/white"
+
+        return True, ""
+
+    def gtp_genmove(self, args):
+        """
+        automatically detect who is to play
+        :param args:
+        :return:
+        """
+        raw_move=self.agent.generate_move()
+        return True, raw_move
+
+    def gtp_show(self, args):
+        return True, state_to_str(self.agent.game_state)
+
+    def gtp_gamestatus(self, args):
+        status=self.agent.gamestatus()
+        if status==-1:
+            return True, "UNSETTLED"
+        elif status==0:
+            return True, "BLACK WIN"
+        elif status==1:
+            return True, "WHITE WIN"
+        else: return False, "ERROR GAME STATUS"
---
+++ tournament/tournament.py
@@ -0,0 +1,73 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import sys
+sys.path.append("..")
+from game_util import *
+import argparse
+from program import Program
+import threading
+import time
+import sys
+
+
+class wrapper_agent(object):
+
+    def __init__(self, executable):
+        self.executable=executable
+        self.program=Program(self.executable, True)
+        self.name=self.program.sendCommand("name").strip()
+        self.lock=threading.Lock()
+
+    def sendCommand(self, command):
+        self.lock.acquire()
+        answer=self.program.sendCommand(command)
+        self.lock.release()
+        return answer
+
+    def reconnect(self):
+        self.program.terminate()
+        self.program=Program(self.executable, True)
+        self.lock=threading.Lock()
+
+
+
+def run_single_match(black_agent, white_agent, verbose=False):
+    game=[]
+    winner=None
+
+    black_agent.sendCommand("clear_board")
+    white_agent.sendCommand("clear_board")
+
+    black=0
+    white=1
+    while True:
+        move =black_agent.sendCommand("genmove black").strip()
+        if move == "resign":
+            winner=white
+            return winner
+        game.append(raw_move_to_int(move))
+        white_agent.sendCommand("play black "+move)
+        if verbose:
+            pass
+
+        sys.stdou.flush()
+        move=white_agent.sendCommand("genmove white").strip()
+        if move=="resign":
+            winner=black
+            return winner
+        game.append(raw_move_to_int(move))
+        black_agent.sendCommand("play white "+move)
+        if verbose:
+            pass
+        sys.stdout.flush()
+
+    winner_name = "black " if winner==black else "white"
+    print("WIN BY "+winner_name)
+    return winner
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description="tournament between agents")
+    parser.add_argument("num_games", type=int, help="num of paired games playing")
+    parser.add_argument("--verbose","-v",action="stroe_consnat", const=True, default=False, )

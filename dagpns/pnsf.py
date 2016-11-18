3993bb41cfee3bf2c06073210dfbbd771c99da41
Chao <cgao3@ualberta.ca>
2016-11-17 17:13:19 -0700

    need to fix pnsf.py

--- dagpns/pnsf.py
+++ dagpns/pnsf.py
@@ -4,8 +4,9 @@ from __future__ import absolute_import
 
 from utils.unionfind import *
 from zobrist.zobrist import *
-from .pns import winner,updateUF
+from dagpns.pns import winner,updateUF
 
+import Queue
 import copy
 
 INF = 2000000000.0
@@ -20,20 +21,22 @@ EPSILON=1e-5
 
 
 class PNSNode:
-    def __init__(self, phi, delta, isexpanded, parents=None):
+    def __init__(self, phi, delta, isexpanded, parents=None, children=None):
         self.phi=phi
         self.delta=delta
         self.isexpanded=isexpanded
         self.parents=parents
+        self.children=children
 
+    def asExpanded(self):
+        self.isexpanded=True
 
 class PNSF:
 
     def __init__(self):
         self.mToplay=None
-        self.mWorkstate=None
+        self.mState=None
         self.zsh=ZobristHash(BOARD_SIZE)
-
 
     def evaluate(self, moveseq):
         blackUF=unionfind()
@@ -47,7 +50,7 @@ class PNSF:
             if(outcome!=self.mToplay):
                 print("outcome ", outcome, "toplay: ", self.mToplay)
                 print(moveseq)
-            return outcome, True
+            return outcome
         for m in range(BOARD_SIZE**2):
             if m not in moveseq:
                 b,w=copy.deepcopy(blackUF), copy.deepcopy(whiteUF)
@@ -57,124 +60,152 @@ class PNSF:
                 if res!=HexColor.EMPTY:
                     assert(res==self.mToplay)
                     moveseq.remove(m)
-                    return res, False
+                    return res
                 moveseq.remove(m)
-        return HexColor.EMPTY, False
+        return HexColor.EMPTY
 
     def pns(self, rootstate, toplay):
+        self.mToplay=toplay
         val=self.evaluate(rootstate)
         if val!=HexColor.EMPTY:
             print("Winner ", val)
             return
-        self.mWorkHash=self.mWorkHash=self.zsh.get_hash(rootstate)
-        root=PNSNode(phi=0, delta=0, isexpanded=False, parents=[])
-        self.mToplay=toplay
-        if val==toplay:
-            root.phi, root.delta=(0,INF)
-        elif val==HexColor.EMPTY-toplay:
-            root.phi, root.delta=(INF,0)
-        else:
-            root.phi, root.delta=(1,1)
         self.node_cnt=0
         self.mTT={}
-        self.mWorkstate=rootstate
-        root=PNSNode(phi=1, delta=1, isexpanded=False)
-        self.mCurrentNode=copy.copy(root)
-        self.mAvailableMoves=[i for i in range(BOARD_SIZE**2) if i not in self.mWorkstate]
-        self.mLeafsTrans=[]
-        while(root.phi > EPSILON and root.delta >EPSILON):
+        rootHash=self.zsh.get_hash(rootstate)
+        self.mState=rootstate
+        rootNode=PNSNode(phi=1, delta=1, isexpanded=False, parents=[])
+        self.tt_write(rootHash, rootNode)
+        ite=0
+        while(rootNode.phi > EPSILON and rootNode.delta >EPSILON):
+            self.mHash = rootHash
+            self.mState = copy.deepcopy(rootstate)
+            self.mToplay = toplay
+            self.mNode = self.tt_lookup(self.mHash)
+            print("iteration ", ite, "root.phi, root.delta", rootNode.phi, rootNode.delta)
+            print("root ", self.mHash)
             self.selection()
-            self.expand()
-            self.mLeafsTrans=[]
+            print("selected: ", self.mHash)
+            self.expansion()
+            print("after expansion  \n")
             self.update_ancesotrs()
+            ite +=1
+            #if ite>3: break
+            rootNode=self.tt_lookup(rootHash)
 
     def tt_lookup(self, code):
         if code in self.mTT.keys():
             return self.mTT[code]
-        return None
+        return False
 
     def tt_write(self, code, n):
         self.mTT[code]=n
 
-    def phiSum(self):
+    def phiSum(self, n):
         s=0
-        for i in self.mAvailableMoves:
-            child_code=self.zsh.update_hash(self.mWorkHash, i, self.mToplay)
+        for child_code,move in n.children:
             node=self.tt_lookup(child_code)
+            k = len(node.parents)
+            if k == 0: k = 1
             assert(node)
-            s += node.phi/len(node.parents)
+            s += node.phi/k
         return s
 
-    def deltaMin(self):
+    def deltaMin(self, n):
         min_delta=INF
-        for i in self.mAvailableMoves:
-            child_code = self.zsh.update_hash(self.mWorkHash, i, self.mToplay)
+        for child_code,move in n.children:
             node = self.tt_lookup(child_code)
             assert (node)
-            min_delta=min(min_delta, node.delta/len(node.parents))
+            k=len(node.parents)
+            if k==0: k=1
+            min_delta=min(min_delta, node.delta/k)
         return min_delta
 
     def selection(self):
-
-        while self.mCurrentNode.isexpanded:
+        print("start selection..")
+        while self.mNode.isexpanded:
             best_move=-1
             best_val=INF
             best_child=None
-            for i in self.mAvailableMoves:
-                code=self.zsh.update_hash(self.mWorkHash, i, self.mToplay)
-                node=self.tt_lookup(code)
+            best_child_code=None
+            for child_code,move in self.mNode.children:
+                node=self.tt_lookup(child_code)
                 assert(node)
-                if node.delta > best_val:
+                if node.delta < best_val:
                     best_val = node.delta
-                    best_move = i
+                    best_move = move
                     best_child=node
+                    best_child_code=child_code
             assert(best_move>-1)
-            self.mWorkstate.append(best_move)
-            self.mWorkHash=self.zsh.update_hash(self.mWorkHash, best_move, self.mToplay)
+            self.mState.append(best_move)
+            self.mHash=best_child_code
             self.mToplay = HexColor.EMPTY - self.mToplay
-            self.mAvailableMoves.remove(best_move)
-            self.mCurrentNode = best_child
-
-
-    def expand(self):
-        assert(self.mCurrentNode.isexpanded==False)
-        for i in self.mAvailableMoves:
-            code=self.zsh.update_hash(code=self.mWorkHash, intmove=i, intplayer=self.mToplay)
+            self.mNode=best_child
+            print("best-child", best_child_code)
+        print("end seelction")
+    def expansion(self):
+        assert(self.mNode.isexpanded==False)
+        self.mNode.asExpanded()
+        avail_moves=[i for i in range(BOARD_SIZE**2) if i not in self.mState]
+        self.mNode.children=[]
+        existing=0
+        self.mExisting=[]
+        for m in avail_moves:
+            child_code=self.zsh.update_hash(code=self.mHash, intmove=m, intplayer=self.mToplay)
+            self.mNode.children.append((child_code,m))
             #check if the leaf has been there
-            n=self.tt_lookup(code)
-            if n:
-                n.parents.append(i)
-                self.tt_write(code, n)
-                self.mLeafsTrans.append(code)
+            node=self.tt_lookup(child_code)
+            if node:
+                node.parents.append(self.mHash)
+                self.tt_write(child_code, node)
+                self.mExisting.append(node)
+                existing +=1
                 continue
-            assert(n==None)
-            self.mWorkstate.append(i)
+            assert(node==False)
+            self.mState.append(m)
             self.mToplay = HexColor.EMPTY - self.mToplay
-            res=self.evaluate(self.mWorkstate)
+            res=self.evaluate(self.mState)
             if res!=HexColor.EMPTY:
                 phi,delta=(0,INF) if res==self.mToplay else (INF, 0)
                 leaf=PNSNode(phi=phi, delta=delta, isexpanded=True)
-                leaf.parents=[i]
-                self.tt_write(code, leaf)
+                leaf.parents=[self.mHash]
+                self.tt_write(child_code, leaf)
                 self.node_cnt +=1
                 if delta == 0: break
             else:
                 phi,delta=(1,1)
                 leaf=PNSNode(phi=phi, delta=delta, isexpanded=False)
-                leaf.parents=[i]
-                self.tt_write(code, leaf)
+                leaf.parents=[self.mHash]
+                self.tt_write(child_code, leaf)
                 self.node_cnt +=1
-            self.mWorkstate.remove(i)
+            self.mState.remove(m)
             self.mToplay = HexColor.EMPTY - self.mToplay
+        self.tt_write(self.mHash, self.mNode)
+        print("expanding ", self.mHash, "has", len(self.mNode.children), "nodes, ", existing, "existing")
 
     def update_ancesotrs(self):
-        if len(self.mLeafsTrans)==0:
-            pass
-        else:
-            pass
-        while True:
-            oldPhi, oldDelta=self.mCurrentNode.phi, self.mCurrentNode.delta
-            self.phiSum()
-            self.deltaMin()
+        Q = Queue.Queue()
+        Q.put(self.mHash)
+        if len(self.mExisting)>0:
+            for c_code in self.mExisting:
+                c_node=self.tt_lookup(c_code)
+                assert(c_node)
+                for p_code in c_node.parents:
+                    if p_code not in Q.queue:
+                        Q.put(p_code)
+        while not Q.empty():
+            code=Q.get()
+            self.mNode=self.tt_lookup(code)
+            oldphi, olddelta=self.mNode.phi, self.mNode.delta
+            self.mNode.phi, self.mNode.delta=self.deltaMin(self.mNode), self.phiSum(self.mNode)
+            self.tt_write(self.mHash, self.mNode)
+            if abs(self.mNode.phi -oldphi) > EPSILON or abs(self.mNode.delta-olddelta) >EPSILON:
+                self.tt_write(code, self.mNode)
+                for p_code in self.mNode.parents:
+                    if p_code not in Q.queue: Q.put(p_code)
 
-        pass
+if __name__ == "__main__":
+    pnsf=PNSF()
+    s=[]
+    toplay=HexColor.BLACK if len(s)%2==0 else HexColor.WHITE
+    pnsf.pns(s, toplay)

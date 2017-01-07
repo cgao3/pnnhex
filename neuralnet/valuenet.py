dafb5ff8ee3696bc39fdc2a07a344a1d07e82d3e
Chao <cgao3@ualberta.ca>
2017-01-07 01:34:50 -0700

    change to 0-1 database

--- neuralnet/valuenet.py
+++ neuralnet/valuenet.py
@@ -166,9 +166,9 @@ def main(argv=None):
         vnet.inference(FLAGS.value_model_path)
         return
 
-    vnet=ValueNet2(srcTrainDataPath="storage/position-value/Train.txt",
-                   srcTestDataPath="storage/position-value/Validate.txt",
-                   srcTestPathFinal="storage/position-value/Test.txt")
+    vnet=ValueNet2(srcTrainDataPath="storage/position-value/8x8/train.txt",
+                   srcTestDataPath="storage/position-value/8x8/validate.txt",
+                   srcTestPathFinal="storage/position-value/8x8/test.txt")
 
     if tf.gfile.Exists(FLAGS.summaries_dir2):
         tf.gfile.DeleteRecursively(FLAGS.summaries_dir2)
